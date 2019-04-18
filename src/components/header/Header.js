e5e5998bf531e99ec2e85ac4752bc8a407b4644b
GravisUltrasound <keshyin@yandex.ru>
2019-04-18 23:43:32 +0300

    Added some design features

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ import s from './Header.module.css';
 const Header = () => {
     return(
       <header className={s.appHeader}>
-            forum for your soul
+            <img src="logo.png"/>
       </header>
     );
 }
