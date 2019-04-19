2920fec897f4f078f6cdd4721612a55a1c3b7597
GravisUltrasound <keshyin@yandex.ru>
2019-04-19 12:10:52 +0300

    Added profile features

---
+++ src/components/profile/profileInfo/ProfileInfo.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Information from './information/Information'
+import s from './ProfileInfo.module.css'
+
+const ProfileInfo = () =>{
+    return(
+        <div className={s.appProfile}>
+            <Avatar/>
+            <Nickname/>
+            <Information/>
+        </div>
+    )
+}
+
+const Avatar = (props) =>{
+    return(
+        <div className={s.appAvatar}>
+            <img width="100" height="100" src="/avatar.png"/>
+        </div>
+    )
+}
+
+const  Nickname = (props) =>{
+    return(
+        <div className={s.appNick}>
+            /prop Nick/
+        </div>
+    )
+}
+
+export default ProfileInfo;
---
+++ src/components/profile/profileInfo/information/Information.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Information.module.css';
+
+const Information = () => {
+    return(
+        <div className={s.appInformation}>
+            ааа
+        </div>
+    )
+}
+
+export default Information;
